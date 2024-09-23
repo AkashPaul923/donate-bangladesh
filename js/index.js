05b73002bb2980d894398cfec5fad983a24bd8be
AkashPaul923 <akashpaul923@gmail.com>
2024-09-23 22:47:45 +0600

    balance updated

--- js/index.js
+++ js/index.js
@@ -4,5 +4,12 @@ function moneyDonateNow(id1 ,id2){
     const inputBalance = parseFloat(document.getElementById(id1).value)
     const campBalance = parseFloat(document.getElementById(id2).innerText)
     const myBalance = parseFloat(document.getElementById('my-balance').innerText)
+
+    // update balance
+    const newCampBalance = campBalance + inputBalance
+    document.getElementById(id2).innerText = newCampBalance
+
+    const newMyBalance = myBalance - inputBalance
+    document.getElementById('my-balance').innerText = newMyBalance
     console.log(inputBalance,campBalance ,myBalance);
 }
